a07b262885dc2fb3a12bd56831b81312d66691ec
Lucien <ldjvalstar@gmail.com>
2020-03-07 20:33:11 +0100

    Updated to new requirements:
    - added pre loop wait
    - loop starts with red, then wait, then yellow and then second wait

--- lucien_code_v2/lucien_code_v2.ino
+++ lucien_code_v2/lucien_code_v2.ino
@@ -1,4 +1,3 @@
-
 // Description:
 // input 1 is a scanner trigger
 // input 2 is a lick detector
@@ -10,116 +9,174 @@
 // then, output 1 (red LED) is turned on for 2 seconds, then off for 6 seconds,
 // then, output 2 (yellow LED) is turned on for 2 seconds, then off for 6 seconds,
 // if input 2 (button 2) is pressed while red LED is on (+ 2 seconds extra),
-// then output 3 (green LED) is turned on for x seconds.
+// then output 3 (green LED) is turned on for 2 seconds.
 
 // Enums and classes:
-enum LoopState {
-  NOT_STARTED,
-  FIRST_WAIT,
-  RED_LIGHT_ON,
-  SECOND_WAIT,
-  YELLOW_LIGHT_ON    
+enum LoopState { // LoopState variable has 5 different values because we declare it as enum (enumeration)
+    NOT_STARTED, // state when the button is not pressed
+    PRE_LOOP_WAIT, // state when the button pressed, but the loop hasn't started. Will start after 6 seconds.
+    LOOP_START,   // state indicating the loop is eligible to start
+    FIRST_LIGHT_ON, // state after the 6 seconds delay is applied and the red LED is now on for 2 seconds
+    FIRST_WAIT, // state after the button is pressed and the 6 seconds delay is applied
+    SECOND_LIGHT_ON, // state after the 6 seconds delay is applied and the yellow LED is now on for 2 seconds
+    SECOND_WAIT // state after the red LED is on and now the 6 seconds delay is applied
 };
 
-// Constants:
-const int LED1 = 6; //red LED, output 1
-const int LED2 = 5; //yellow LED, output 2
-const int LED3 = 4; //green LED, output 3
-const int BUTTON_1 = 3; //input 1
-const int BUTTON_2 = 2; //input 2
-const int DELAY_SHORT_MILLIS = 2000;
-const int DELAY_MEDIUM_MILLIS = 4000;
-const int DELAY_LONG_MILLIS = 6000;
-const int COMPLETE_LOOP_MILLIS = 3 * DELAY_MEDIUM_MILLIS + 2 * DELAY_SHORT_MILLIS;
-const int MAX_LOOP_ITERATIONS = 4;
+// Constants that won't change:
+const int LED1 = 6; // red LED, output 1
+const int LED2 = 5; // yellow LED, output 2
+const int LED3 = 4; // green LED, output 3
+const int BUTTON_1 = 3; // input 1
+const int BUTTON_2 = 2; // input 2
+const int DELAY_SHORT_MILLIS = 2000; // give a delay of 2 seconds
+const int DELAY_MEDIUM_MILLIS = 4000; // give a delay of 4 seconds
+const int DELAY_LONG_MILLIS = 6000; // give a delay of 6 seconds
+const int MAX_LOOP_ITERATIONS = 4; // the maximum number of repeat in the loop is 5 times
+const int COMPLETE_LOOP_MILLIS = 2 * DELAY_LONG_MILLIS + 2 * DELAY_SHORT_MILLIS;
+const int PRINT_RATE_MILLIS = 2000;
 
 // Variable definitions:
-int loopCount = 0;
-int currentLoopStartTime = -1;
-LoopState loopState = NOT_STARTED;
+int loopCount = 0; // to declare the number of loop
+unsigned long currentLoopStartTime = 1; // to declare the current loop start time
+LoopState loopState = NOT_STARTED; // to declare the loop state
 
-bool led3IsOn = false;
-int led3TurnedOnTime = -1;
+bool led3IsOn = false; // to declare the variable while LED 3 is on
+int led3TurnedOnTime = 1; // to declare the timing while LED 3 is on.
+
+unsigned long buttonOnePressTime = 0;
+unsigned long timeSinceProgramStart = 0;
+unsigned long lastPrintTime = 0;
 
 // Program code:
 void setup() {
-    pinMode(LED1, OUTPUT); //set LED1 (red) as an output 1
-    pinMode(LED2, OUTPUT); //set LED2 (yellow) as an output 2
-    pinMode(LED3, OUTPUT); //set LED3 (green) as an output 3
-    pinMode(BUTTON_1, INPUT); //set button 1 as an input 1
-    pinMode(BUTTON_2, INPUT); //set button 2 as an input 2
 
-    reset();
+    Serial.begin(9600); // to see values from Arduino and present it on the computer
+                                 
+    pinMode(LED1, OUTPUT); // set LED1 (red) as an output 1
+    pinMode(LED2, OUTPUT); // set LED2 (yellow) as an output 2
+    pinMode(LED3, OUTPUT); // set LED3 (green) as an output 3
+    pinMode(BUTTON_1, INPUT); // set button 1 as an input 1
+    pinMode(BUTTON_2, INPUT); // set button 2 as an input 2
+
+    reset(); // reset all conditions
 }
 
 void loop() {
 
-    if (buttonIsPressed(BUTTON_1) && loopState == NOT_STARTED) {
-        loopCount = 0;
-        loopState = FIRST_WAIT;
-        currentLoopStartTime = millis();
+    timeSinceProgramStart = millis();
+  
+    if (buttonIsPressed(BUTTON_1) && loopState == NOT_STARTED) { 
+        // if button 1 is pressed and the loop state is 'not started'
+        printValuesToSerial();
+        loopState = PRE_LOOP_WAIT;
+        buttonOnePressTime = timeSinceProgramStart;
+    }
+
+    if (loopState == PRE_LOOP_WAIT && (timeSinceProgramStart - buttonOnePressTime) >= DELAY_LONG_MILLIS) {
+        printValuesToSerial();
+        loopCount = 0; // then the loop count is set to 0
+        loopState = LOOP_START; // and the loop is eligible to start
+        currentLoopStartTime = timeSinceProgramStart; // and the current loop start time begins and is set to miliseconds
     }
  
-    if (loopState != NOT_STARTED && loopCount < MAX_LOOP_ITERATIONS) {
+    if (loopHasStarted() && loopCount < MAX_LOOP_ITERATIONS) { 
+        printValuesToSerial();
       
-        int timeInCurrentLoop = (millis() - currentLoopStartTime);
+        // if the loop state 'starts' and the number of loop count is less than the max number of loop repeat
+        int timeInCurrentLoop = (timeSinceProgramStart - currentLoopStartTime); // then the time in current loop is calculated from the number of current miliseconds substracted with the miliseconds of the starting loop
 
-        if (loopState == FIRST_WAIT && timeInCurrentLoop >= DELAY_LONG_MILLIS) {
-            turnLedOn(LED1);
-            loopState = RED_LIGHT_ON;        
+        if (loopState == LOOP_START) {
+            turnLedOn(LED1); // red LED is on
+            loopState = FIRST_LIGHT_ON;        
         }
 
-        if (loopState == RED_LIGHT_ON && timeInCurrentLoop >= (DELAY_LONG_MILLIS+DELAY_SHORT_MILLIS)) {
-            turnLedOff(LED1);
+        if (loopState == FIRST_LIGHT_ON && timeInCurrentLoop >= DELAY_SHORT_MILLIS) { 
+            turnLedOff(LED1); // red LED is off 
+            loopState = FIRST_WAIT; // and the loop state switches to 'first wait'. The first wait lasts for 6 seconds.
+        }
+
+        if (loopState == FIRST_WAIT && timeInCurrentLoop >= (DELAY_LONG_MILLIS + DELAY_SHORT_MILLIS)) { // when the loop state is 'first wait' and the time in current loop is bigger than 8 seconds
+            turnLedOn(LED2); // yellow LED is on
+            loopState = SECOND_LIGHT_ON; // and the loop state switches to 'yellow light on'. The yellow LED is on for 2 seconds
+        }
+
+        if (loopState == SECOND_LIGHT_ON && timeInCurrentLoop >= (DELAY_LONG_MILLIS + 2*DELAY_SHORT_MILLIS)) { // when the loop state is 'second light on' and the time in current loop is bigger than 10 seconds
+            turnLedOff(LED2); // yellow LED is off
             loopState = SECOND_WAIT;
         }
 
-        if (loopState == SECOND_WAIT && timeInCurrentLoop >= (2*DELAY_LONG_MILLIS + DELAY_SHORT_MILLIS)) {
-            turnLedOn(LED2);
-            loopState = YELLOW_LIGHT_ON;
+        if (loopState == SECOND_WAIT && timeInCurrentLoop >= COMPLETE_LOOP_MILLIS) { // loop is completed
+            loopCount += 1; // loop count is incremented by +1 (another round of loop starts again).
+            loopState = LOOP_START; // loop goes back to 'first wait' state
+            currentLoopStartTime = millis(); // and the current loop start time begins again and is set to miliseconds
         }
 
-        if (loopState == YELLOW_LIGHT_ON && timeInCurrentLoop >= (2*DELAY_LONG_MILLIS + 2*DELAY_SHORT_MILLIS)) {
-            turnLedOff(LED2);
-
-            loopCount += 1;
-            loopState = FIRST_WAIT;
-            currentLoopStartTime = millis();
+        if (buttonIsPressed(BUTTON_2) && timeInCurrentLoop <= DELAY_MEDIUM_MILLIS) { // if button 2 is pressed and the time in current loop is below 4 seconds
+            turnLedOn(LED3); // green LED is on
+            led3IsOn = true; // green LED on is true
+            led3TurnedOnTime = timeInCurrentLoop; // the time when green LED is on is now the time in current loop
         }
 
-        if (buttonIsPressed(BUTTON_2) && timeInCurrentLoop >= DELAY_LONG_MILLIS && timeInCurrentLoop <= (DELAY_LONG_MILLIS + DELAY_MEDIUM_MILLIS)) {
-            turnLedOn(LED3);
-            led3IsOn = true;
-            led3TurnedOnTime = timeInCurrentLoop;
-        }
-
-        if (led3IsOn == true && (timeInCurrentLoop - led3TurnedOnTime) >= DELAY_SHORT_MILLIS) {
-            turnLedOff(LED3);
-            led3IsOn = false; 
-            led3TurnedOnTime = -1;
+        if (led3IsOn == true && (timeInCurrentLoop - led3TurnedOnTime) >= DELAY_SHORT_MILLIS) { // when the green LED is on and the time in current loop substracted with the time when green LED is on is bigger than 2 seconds
+            turnLedOff(LED3); // green LED is off
+            led3IsOn = false; // green LED on is false
+            led3TurnedOnTime =  1; // the time when green LED is on goes back to the original
         }         
     }
 
-    if (loopCount == MAX_LOOP_ITERATIONS) {
-        loopState = NOT_STARTED;
+    if (loopCount == MAX_LOOP_ITERATIONS) { // if the number of loop reaches the maximum number of loop repeat
+        loopState = NOT_STARTED; // then the state of loop is over
     }
 }
 
-bool buttonIsPressed(int buttonNumber) {
-    int buttonState = digitalRead(buttonNumber); 
+bool buttonIsPressed(int buttonNumber) { // when button x is pressed
+    int buttonState = digitalRead(buttonNumber); // then the state of button x is high
     return buttonState == HIGH;
 }
 
-void turnLedOn(int ledNumber) {
-    digitalWrite(ledNumber, HIGH);
+void turnLedOn(int ledNumber) { // when LED x is on
+    digitalWrite(ledNumber, HIGH); // then the state of LED x is high
 }
 
-void turnLedOff(int ledNumber) {
-    digitalWrite(ledNumber, LOW); 
+void turnLedOff(int ledNumber) { // when LED x is off
+    digitalWrite(ledNumber, LOW);  // then the state of LED x is low
 }
 
-void reset() {
-  
+bool loopHasStarted() {
+   return loopState != NOT_STARTED && loopState != PRE_LOOP_WAIT;  
+}
+
+void printValuesToSerial() {
+
+    if (loopState == NOT_STARTED) {
+        Serial.println("Button 1 has been pressed.");
+    } else if (loopState == PRE_LOOP_WAIT) {
+        Serial.println("Loop has started after pre loop wait");
+        Serial.println("Time in millisecs,Red LED value,Yellow LED value,Green LED value,Button 2 value"); // Then these words are printed 
+    } else { 
+        int redLedValue = digitalRead(LED1); // to read the red LED value from pin 6
+        int yellowLedValue = digitalRead(LED2); // to read the yellow LED value from pin 5
+        int greenLedValue = digitalRead(LED3); // to read the green LED value from pin 4
+        int button2Value = digitalRead(BUTTON_2); // to read the button 2 value from pin 2
+    
+        if(lastPrintTime != timeSinceProgramStart && (timeSinceProgramStart % PRINT_RATE_MILLIS) == 0) {
+            Serial.print("\b"); // print \b as it is in python configuration
+            Serial.print(timeSinceProgramStart); // print the time while the loop is running in milliseconds
+            Serial.print(","); // give a comma
+            Serial.print(redLedValue); // print the red LED value
+            Serial.print(","); // give a comma
+            Serial.print(yellowLedValue); // print the yellow LED value
+            Serial.print(","); // give a comma
+            Serial.print(greenLedValue); // print the green LED value
+            Serial.print(","); // give a comma
+            Serial.println(button2Value); // print the button 2 value
+
+            lastPrintTime = timeSinceProgramStart;
+        }
+    }
+}
+
+void reset() { // when the system is reset, all LEDs are off 
     turnLedOff(LED1);
     turnLedOff(LED2);
     turnLedOff(LED3);  
